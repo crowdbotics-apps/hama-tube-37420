844a19d611022b0bdd3a23edb0388a338d0a879d
Crowdbotics <team@crowdbotics.com>
2022-10-30 04:48:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hama_tube_37420",
+  url: "https://hama_tube_37420.botics.co",
+  api: "https://hama_tube_37420.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
